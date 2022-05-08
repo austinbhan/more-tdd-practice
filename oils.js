5ae1fac7bf0355cd327a4593bcec664ad2fdc31d
austinbhan <austinbhan@outlook.com>
2022-05-08 16:17:46 -0700

    Created target HTML and array file

---
+++ oils.js
@@ -0,0 +1,10 @@
+export const oils = [
+    {  
+        img: './assets/olive.jepg', 
+        ingredient: 'Olive', 
+        smokePoint: 'low', 
+        price: 'low' 
+    },
+    { img: './assets/coconut.jpeg', ingredient: 'Coconut', smokePoint: 'high', price: 'high' },
+    { img: './assets/corn.jpeg', ingredient: 'Corn', smokePoint: 'high', price: 'high' }
+];
